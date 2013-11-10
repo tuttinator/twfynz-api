da6d9258e359d4eb621274f762e62be5e23f4b38
Caleb Tutty <caleb@carnivallabs.com>
2013-11-10 13:43:14 +1300

    Added election factory

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   
   factory :parliament do
-    ordinal_number { Parliament.last.ordinal_number + 1 }
+    ordinal_number { Parliament.last.ordinal_number + 50 }
   end
 
   factory :party do
@@ -12,6 +12,14 @@ FactoryGirl.define do
     name 'Auckland Central'
     island 'North Island'
     maori false
+  end
+
+  factory :election do
+    voting_commenced_on Date.new(2011, 11, 26), 
+    voting_finished_on Date.new(2011, 11, 26), 
+    parliament
+    registered_voters 3_070_847, 
+    percentage_turnout 74.2
   end
 
 end
